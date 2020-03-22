7da674a07d11617addcd5a1a03435cc96cb1d72b
Prashant Sedhain <prashantased@gmail.com>
2020-03-22 16:42:21 -0500

    added crud components

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CreateComponent } from "./create/create.component";
+import { ReadComponent } from "./read/read.component";
+import { UpdateComponent } from "./update/update.component";
+import { DeleteComponent } from "./delete/delete.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "createPage", component: CreateComponent },
+  { path: "readPage", component: ReadComponent },
+  { path: "updatePage", component: UpdateComponent },
+  { path: "deletePage", component: DeleteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    ReadComponent,
+    UpdateComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
